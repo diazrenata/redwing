bcca5d72991acec531c1b322716dc0e5834c6211
Renata Diaz <diaz.renatam@gmail.com>
2021-09-02 16:37:01 -0400

    to run

---
+++ pipeline_actual.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(rwar)
+library(drake)
+library(MATSS)
+
+expose_imports(rwar)
+
+
+datasets <- MATSS::build_bbs_datasets_plan()
+
+
+working_datasets <- read.csv(here::here("working_routes.csv"))
+
+datasets <- datasets[ which(datasets$target %in% working_datasets$matssname), ]
+
+
+methods <- drake_plan(
+  results = target(all_core_analyses(dataset, beginyears = c(1988:1992), endyears = c(2014:2018)),
+             transform = map(
+               dataset = !!rlang::syms(datasets$target))),
+  all_results = target(dplyr::combine(results),
+                           transform = combine(results))
+)
+
+all = bind_rows(datasets, methods)
+
+
+## Set up the cache and config
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("drake-cache-actual.sqlite"))
+cache <- storr::storr_dbi("datatable", "keystable", db)
+cache$del(key = "lock", namespace = "session")
+
+
+## Run the pipeline
+nodename <- Sys.info()["nodename"]
+if(grepl("ufhpc", nodename)) {
+  print("I know I am on the HiPerGator!")
+  library(clustermq)
+  options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmpl")
+  ## Run the pipeline parallelized for HiPerGator
+  make(all,
+       force = TRUE,
+       cache = cache,
+       verbose = 1,
+       parallelism = "clustermq",
+       jobs = 20,
+       caching = "main", memory_strategy = "autoclean") # Important for DBI caches!
+} else {
+
+
+  # Run the pipeline on multiple local cores
+  system.time(make(all, cache = cache,  verbose = 1, memory_strategy = "autoclean"))
+
+
+}
+
+
+
+DBI::dbDisconnect(db)
+rm(cache)
+print("Completed OK")
+
