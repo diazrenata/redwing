6b8a66bea5a507f61bcbb7322592c690e373858a
Renata Diaz <diaz.renatam@gmail.com>
2021-10-07 11:14:22 -0400

    run at scale???

---
+++ pipeline-resim.R
@@ -0,0 +1,70 @@
+library(dplyr)
+library(rwar)
+library(drake)
+library(MATSS)
+library(BBSsize)
+
+expose_imports(rwar)
+source(here::here("resim/resim_fxns.R"))
+
+
+datasets <- MATSS::build_bbs_datasets_plan()
+
+
+working_datasets <- read.csv(here::here("working_routes.csv"))
+
+datasets <- datasets[ which(datasets$target %in% working_datasets$matssname), ]
+#datasets <- datasets[1:2, ]
+
+
+methods <- drake_plan(
+  sgmms = target(construct_sampling_gmm(dataset),
+                   transform = map(
+                     dataset = !!rlang::syms(datasets$target))),
+  sims = target(draw_communities_wrapper(ts_comp = dataset, sampling_gmms = sgmms, ndraws =2),
+                transform = map(
+                  sgmms
+                )),
+  ar = target(dplyr::combine(sims),
+              transform = combine(sims)),
+  all_results = target(dplyr::bind_rows(ar))
+)
+
+all = bind_rows(datasets, methods)
+
+
+## Set up the cache and config
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("drake-cache-actual-resim.sqlite"))
+cache <- storr::storr_dbi("datatable", "keystable", db)
+cache$del(key = "lock", namespace = "session")
+
+
+## Run the pipeline
+nodename <- Sys.info()["nodename"]
+if(grepl("ufhpc", nodename)) {
+  print("I know I am on the HiPerGator!")
+  library(clustermq)
+  options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmpl")
+  ## Run the pipeline parallelized for HiPerGator
+  make(all,
+       force = TRUE,
+       cache = cache,
+       verbose = 1,
+       parallelism = "clustermq",
+       jobs = 20,
+       caching = "main", memory_strategy = "autoclean") # Important for DBI caches!
+} else {
+
+
+  # Run the pipeline on multiple local cores
+  system.time(make(all, cache = cache,  verbose = 1, memory_strategy = "autoclean", lock_envir = F))
+
+
+}
+
+
+
+DBI::dbDisconnect(db)
+rm(cache)
+print("Completed OK")
+
--- resim/run.R
+++ resim/run.R
@@ -5,3 +5,16 @@ ts_comp <- BBSsize::granby
 ts_gmms <- construct_sampling_gmm(granby)
 
 gsims <- draw_communities_wrapper(granby, ndraws = 50, sampling_gmms = ts_gmms)
+
+library(ggplot2)
+
+ggplot(gsims, aes(year, total_biomass, color = source, group = as.factor(sim_iteration))) + geom_smooth(se = F, method = "lm") + facet_wrap(vars(source))
+
+ggplot(gsims, aes(year, total_biomass, color = source, group = as.factor(sim_iteration))) + geom_point() + facet_wrap(vars(source))
+
+hartland <- BBSsize::hartland
+h_gmms <- construct_sampling_gmm(hartland, begin_years = 1994:1998)
+
+hsims <- draw_communities_wrapper(hartland, ndraws = 2, sampling_gmms = h_gmms, begin_years = 1994:1998)
+
+ggplot(hsims, aes(year, total_biomass, color = sim_iteration, group = as.factor(sim_iteration))) + geom_smooth(se = F, method = "lm") + facet_wrap(vars(source))
