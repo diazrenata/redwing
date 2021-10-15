5c896d51b4046953e15e05bf67b5658d9922482e
Renata Diaz <diaz.renatam@gmail.com>
2021-10-15 10:01:43 -0400

    poissons consistently don't converge and give misleading fits due to random factor. stick with gaussian.

--- poisson.R
+++ poisson.R
@@ -7,8 +7,8 @@ db <- DBI::dbConnect(RSQLite::SQLite(), here::here("drake-cache-actual-resim.sql
 cache <- storr::storr_dbi("datatable", "keystable", db)
 cache$del(key = "lock", namespace = "session")
 
-loadd(sims_sgmms_bbs_rtrg_14_33, cache = cache)
-some_sims = sims_sgmms_bbs_rtrg_14_33
+loadd(sims_sgmms_bbs_rtrg_116_18, cache = cache)
+some_sims = sims_sgmms_bbs_rtrg_116_18
 
 justsims <- dplyr::filter(some_sims, source %in% c("actual", "sim")) # remove raw
 
@@ -16,13 +16,30 @@ justsims <- justsims %>%
   mutate(total_energy_integer = round(total_energy))
 
 # Fit a brm on total_energy
-te_brm <- brms::brm(total_energy ~ (timeperiod * source) + (1 | year), data = justsims, cores = 4, iter = 8000, thin = 2)
+te_brm <- brms::brm(total_energy_integer ~ (timeperiod * source) + (1 | year), data = justsims, cores = 4, iter = 8000, thin = 2, family = poisson)
 
-summary(te_brm)
+te_brm_draws <- tidy_draws(te_brm)
+te_brm_fit <- predict(te_brm, re_formula = NA) %>% as.data.frame()
 
-loadd(fits_sims_sgmms_bbs_rtrg_23_64, cache = cache)
+te_brm_fit <- cbind(justsims, te_brm_fit)
 
-summary(fits_sims_sgmms_bbs_rtrg_23_64$te_brm)
+ggplot(te_brm_fit, aes(year, total_energy_integer, color = source)) + geom_point() + geom_point(aes(y = Estimate), color = "black") + facet_wrap(vars(source))
 
-# Fit the brm on total_biomass
-tb_brm <- brms::brm(total_biomass ~ (timeperiod * source) + (1 | year), data = justsims, cores = cores, iter = iter)
+loadd(fits_sims_sgmms_bbs_rtrg_116_18, cache = cache)
+te_brm_fit_g <- predict(fits_sims_sgmms_bbs_rtrg_116_18$te_brm, re_formula = NA) %>% as.data.frame()
+te_brm_fit_g <- cbind(justsims, te_brm_fit_g)
+
+
+ggplot(te_brm_fit_g, aes(year, total_energy, color = source)) + geom_point() + geom_point(aes(y = Estimate, color = "black")) + facet_wrap(vars(source))
+
+
+te_brm_sqrt <- brms::brm(sqrt(total_energy) ~ (timeperiod * source) + (1 | year), data = justsims, cores = 4, iter = 8000, thin = 2)
+
+te_brm_sqrt_fit <- predict(te_brm_sqrt, re_formula = NA) %>% as.data.frame()
+
+te_brm_sqrt_fit <- cbind(te_brm_sqrt_fit, justsims)
+
+ggplot(te_brm_sqrt_fit, aes(year, total_energy_integer, color = source)) + geom_point() + geom_point(aes(y = Estimate^2), color = "black") + facet_wrap(vars(source))
+
+
+ggplot(te_brm_fit_g, aes(year, total_energy_integer, color = source)) + geom_point() + geom_point(aes(y = Estimate), color = "black") + facet_wrap(vars(source))
