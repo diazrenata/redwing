b600c8130c734acf5c5c6a7c729bd699452b31ac
Renata Diaz <diaz.renatam@gmail.com>
2021-10-19 15:51:01 -0400

    oops switch to mp

--- aspirational_structure/drake_pipelines/pipeline_route_metadata.R
+++ aspirational_structure/drake_pipelines/pipeline_route_metadata.R
@@ -29,18 +29,31 @@ methods <- drake_plan(
 
 all = bind_rows(datasets, methods)
 
-## Set up the cache and config
-db <- DBI::dbConnect(RSQLite::SQLite(), here::here("aspirational_structure", "drake_caches", "drake-cache-route-metadata.sqlite"))
-cache <- storr::storr_dbi("datatable", "keystable", db)
-cache$del(key = "lock", namespace = "session")
+## Run the pipeline
+nodename <- Sys.info()["nodename"]
+if(grepl("ufhpc", nodename)) {
+  print("I know I am on the HiPerGator!")
+  library(clustermq)
+  options(clustermq.scheduler = "multicore"#, clustermq.template = "slurm_clustermq.tmpl")
+  )
+  ## Run the pipeline parallelized for HiPerGator
+  system.time(make(all,
+                   force = TRUE,
+                   cache = cache,
+                   verbose = 1,
+                   parallelism = "clustermq",
+                   jobs = 5,
+                   caching = "main",
+                   memory_strategy = "autoclean",
+                   lock_envir = F))# Important for DBI caches!
+} else {
 
 
-# Run the pipeline on multiple local cores
-system.time(make(all, cache = cache,  verbose = 1, memory_strategy = "autoclean"))
+  # Run the pipeline on multiple local cores
+  system.time(make(all, cache = cache,  verbose = 1, memory_strategy = "autoclean", lock_envir = F))
 
 
-
-
+}
 
 DBI::dbDisconnect(db)
 rm(cache)
