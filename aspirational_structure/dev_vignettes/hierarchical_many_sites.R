883527b56b2f9c71fdc6a6056ed4cd9dc83680b9
Renata Diaz <diaz.renatam@gmail.com>
2021-10-22 16:19:57 -0400

    send this to hpg

--- aspirational_structure/dev_vignettes/hierarchical_many_sites.R
+++ aspirational_structure/dev_vignettes/hierarchical_many_sites.R
@@ -15,15 +15,24 @@ print("Completed OK")
 
 set.seed(1989)
 
-shorter_sites <- sample(unique(all_sims$matssname), size = 100, replace = F)
+shorter_sites <- sample(unique(all_sims$matssname), size = 3, replace = F)
 
 shorter_sims <- filter(all_sims, as.numeric(sim_iteration) < 6, matssname %in% shorter_sites)
 
 set.seed(NULL)
 
-hbrm <- brm(total_energy ~ 0 + (timeperiod * source | matssname), data = shorter_sims, cores = 4)
+print(Sys.time())
+hbrm <- brm(total_energy ~ 0 + ((timeperiod * source) | matssname), data = shorter_sims, cores = 4)
+
+hbrm1 <- brm(total_energy ~ 0 + ((timeperiod * source) | matssname), data = shorter_sims, cores = 4, iter = 10000)
+
+print(Sys.time())
 
 hbrm_post <- posterior_samples(hbrm)
+hbrm_post1 <- posterior_samples(hbrm1)
 
-save(hbrm, file = "hbrm.Rds")
-save(hbrm_post, file = "hbrm_post.Rds")
+save(hbrm, file = here::here("aspirational_structure", "dev_vignettes", "hbrm.Rds"))
+save(hbrm_post, file = here::here("aspirational_structure", "dev_vignettes", "hbrm_post.Rds"))
+
+save(hbrm1, file = here::here("aspirational_structure", "dev_vignettes", "hbrm1.Rds"))
+save(hbrm_post1, file = here::here("aspirational_structure", "dev_vignettes", "hbrm_post1.Rds"))
