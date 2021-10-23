875ca846b69c70067b689a819d334f3fdbb6564f
Renata Diaz <diaz.renatam@gmail.com>
2021-10-23 13:06:26 -0400

    try all sites 30k it

--- aspirational_structure/dev_vignettes/hierarchical_many_sites.R
+++ aspirational_structure/dev_vignettes/hierarchical_many_sites.R
@@ -1,26 +1,26 @@
 library(dplyr)
 library(brms)
-# library(drake)
-#
-#
-# ## Set up the cache and config
-# db <- DBI::dbConnect(RSQLite::SQLite(), here::here("old_caches", "drake-cache-actual-resim.sqlite"))
-# cache <- storr::storr_dbi("datatable", "keystable", db)
-# cache$del(key = "lock", namespace = "session")
-#
-# loadd(all_sims, cache = cache)
-#
-#
-# DBI::dbDisconnect(db)
-# rm(cache)
-# print("Completed OK")
-#
-# set.seed(1989)
-#
-# shorter_sites <- sample(unique(all_sims$matssname), size = 200, replace = F)
-#
-# shorter_sims <- filter(all_sims, as.numeric(sim_iteration) < 6, matssname %in% shorter_sites, source != "raw")
-# write.csv(shorter_sims,here::here("aspirational_structure", "dev_vignettes", "shorter_sims.csv"), row.names = F)
+library(drake)
+
+
+## Set up the cache and config
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("old_caches", "drake-cache-actual-resim.sqlite"))
+cache <- storr::storr_dbi("datatable", "keystable", db)
+cache$del(key = "lock", namespace = "session")
+
+loadd(all_sims, cache = cache)
+
+
+DBI::dbDisconnect(db)
+rm(cache)
+print("Completed OK")
+
+set.seed(1989)
+
+#shorter_sites <- sample(unique(all_sims$matssname), size = 200, replace = F)
+
+shorter_sims <- filter(all_sims, as.numeric(sim_iteration) < 2, matssname %in% shorter_sites, source != "raw")
+write.csv(shorter_sims,here::here("aspirational_structure", "dev_vignettes", "shorter_sims.csv"), row.names = F)
 
 shorter_sims <- read.csv(here::here("aspirational_structure", "dev_vignettes", "shorter_sims.csv"))
 set.seed(NULL)
@@ -28,7 +28,7 @@ set.seed(NULL)
 print(Sys.time())
 #hbrm <- brm(total_energy ~ 0 + ((timeperiod * source )| matssname), data = shorter_sims, cores = 4)
 
-hbrm1 <- brm(total_energy ~ 0 + ((timeperiod * source) | matssname), data = shorter_sims, cores = 4, iter = 20000)
+hbrm1 <- brm(total_energy ~ 0 + ((timeperiod * source) | matssname), data = shorter_sims, cores = 4, iter = 30000)
 
 print(Sys.time())
 
