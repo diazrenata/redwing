ef2c961eb4d826fc405cc1f41411b959d4f4a92f
Renata Diaz <diaz.renatam@gmail.com>
2021-10-25 00:52:43 -0400

    run this and if it goes increase n

--- aspirational_structure/dev_vignettes/zero_interept.R
+++ aspirational_structure/dev_vignettes/zero_interept.R
@@ -20,3 +20,13 @@ justsims <- filter(actual_sims_bbs_rtrg_318_3, source != "raw") %>%
   ungroup()
 
 a_brm <- brm(total_biomass ~ timeperiod * source, data = justsims, prior = prior(constant(0), class = "b", coef = "sourcecurrency") )
+
+a_brm1 <- brm(total_biomass ~ timeperiod * source, data = justsims )
+
+
+ggplot(justsims, aes(year, total_biomass, color = source)) + geom_point()
+
+justsims_p <- justsims %>%
+  mutate(predicted = predict(a_brm)[,1],
+         predicted_withint = predict(a_brm1)[,1])
+ggplot(justsims_p, aes(year, total_biomass, color = source)) + geom_point()+ geom_point(aes(y = predicted)) + geom_point(aes(y = predicted_withint), shape = 3)
