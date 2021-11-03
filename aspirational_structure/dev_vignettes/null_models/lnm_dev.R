f8726eb23c57dc2ff64a1bd6a39e4f6e189ef25c
Renata Diaz <diaz.renatam@gmail.com>
2021-11-03 18:10:18 -0400

    when you get the chance run this

--- aspirational_structure/dev_vignettes/null_models/lnm_dev.R
+++ aspirational_structure/dev_vignettes/null_models/lnm_dev.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(BBSsize)
 library(rwar)
+library(ggplot2)
 g <- MATSS::get_bbs_route_region_data(route = 224, region  =3)
 local_null_model <- function(ts_dat, null_mod_seed = 1989, sim_index = 1, begin_years = 1988:1992, end_years = 2014:2018, isd_seed = 1989, n_isd_draws = 10, ndraws = 100) {
 
@@ -12,6 +13,14 @@ local_null_model <- function(ts_dat, null_mod_seed = 1989, sim_index = 1, begin_
   shuffled_dat <- shuffle_local(ts_dat = ts_dat, null_mod_seed = null_mod_seed)
 
   null_sims <- ssims_wrapper(shuffled_dat, initial_isd_seed_gmm =isd_seed, initial_draw_seed = null_mod_seed, n_isd_draws = n_isd_draws, ndraws = ndraws, simtype = "actual")
+
+  total_abund <- data.frame(
+    year = ts_dat$covariates$year,
+    total_abundance = rowSums(ts_dat$abundance)
+  ) %>%
+    dplyr::filter(year %in% c(1988:1992, 2014:2018))
+
+  null_sims <- dplyr::left_join(null_sims, total_abund)
 
   results <- null_sims %>%
     dplyr::mutate(
@@ -25,10 +34,10 @@ local_null_model <- function(ts_dat, null_mod_seed = 1989, sim_index = 1, begin_
 }
 
 
-nm <- local_null_model(g, n_isd_draws = 2, ndraws= 2)
+#nm <- local_null_model(g, n_isd_draws = 2, ndraws= 2)
 
-nm2 <- local_null_model(g, n_isd_draws = 2, ndraws= 2, null_mod_seed = 1990)
-nm3 <- local_null_model(g, n_isd_draws = 2, ndraws= 2, null_mod_seed = 1990)
+#nm2 <- local_null_model(g, n_isd_draws = 2, ndraws= 2, null_mod_seed = 1990)
+#nm3 <- local_null_model(g, n_isd_draws = 2, ndraws= 2, null_mod_seed = 1990)
 
 
 local_null_model_wrapper <- function(ts_dat, n_null_model_sims = 100, begin_years = 1988:1992, end_years = 2014:2018, isd_seed = 1989, n_isd_draws = 10, ndraws = 100, initial_null_model_seed = 1989) {
@@ -48,6 +57,52 @@ local_null_model_wrapper <- function(ts_dat, n_null_model_sims = 100, begin_year
 
 }
 
-nm4 <- local_null_model_wrapper(g, n_null_model_sims = 5, n_isd_draws = 2, ndraws = 2)
+nm4 <- local_null_model_wrapper(g, n_null_model_sims = 5, n_isd_draws = 5, ndraws = 25)
 
-ggplot(nm4, aes(year, total_biomass, color = source)) + geom_point()
+ggplot(nm4, aes(year, total_biomass, color = source)) + geom_point() + facet_wrap(vars(null_mod_seed)) + geom_smooth(method = "lm", se = F)
+
+summarize_null_models <- function(nm_results) {
+  nm_summary <- nm_results %>%
+    dplyr::mutate(mean_energy = total_energy / total_abundance,
+                  mean_biomass = total_biomass / total_abundance) %>%
+    dplyr::group_by(matssname, simtype,null_mod_type,  null_mod_seed, timeperiod, source) %>%
+    summarize(mean_energy = mean(mean_energy),
+              mean_biomass = mean(mean_biomass)) %>%
+    ungroup() %>%
+    tidyr::pivot_longer(-c(matssname, simtype, null_mod_type, null_mod_seed, timeperiod, source), names_to = "e_or_b", values_to = "avg") %>%
+    tidyr::pivot_wider(id_cols = c(matssname, simtype,null_mod_type,  null_mod_seed, e_or_b, timeperiod), names_from = source, values_from = avg) %>%
+    dplyr::mutate(ratio = currency / abundance) %>%
+    tidyr::pivot_wider(id_cols = c(matssname, simtype, null_mod_type, null_mod_seed, e_or_b), names_from = timeperiod, values_from = ratio) %>%
+    dplyr::mutate(time_ratio = end / begin)
+
+  nm_summary
+}
+
+#' Get actual values for null
+#'
+#' @param dat d
+#' @param simtype s
+#' @param initial_isd_seed_gmm s
+#' @param n_isd_draws s
+#' @param ndraws s
+#' @param initial_draw_seed s
+#'
+#' @return
+#' @export
+#'
+#' @importFrom dplyr mutate
+null_model_actual <- function(dat, simtype = "actual", initial_isd_seed_gmm = 1989, n_isd_draws = 10, ndraws = 100, initial_draw_seed = 1989) {
+
+  actual_sims <- ssims_wrapper(dat, simtype = simtype, initial_isd_seed_gmm = initial_isd_seed_gmm, n_isd_draws = n_isd_draws,
+                                ndraws = ndraws, initial_draw_seed = initial_draw_seed)
+
+
+  actual_sims <- actual_sims %>%
+    dplyr::mutate(
+      null_mod_type = "real",
+      null_mod_seed = -99
+    )
+
+  return(actual_sims)
+
+}
---
+++ aspirational_structure/pipelines/pipeline_lnm_dev.R
@@ -0,0 +1,105 @@
+library(dplyr)
+#library(rwar)
+library(drake)
+library(MATSS)
+library(BBSsize)
+library(brms)
+library(tidybayes)
+
+run_hpg = T
+
+datasets <- MATSS::build_bbs_datasets_plan()
+
+
+working_datasets <- read.csv(here::here("aspirational_structure", "supporting_data", "working_routes.csv"))
+
+
+datasets <- datasets[ which(datasets$target %in% working_datasets$matssname), ]
+
+#datasets <- datasets[ unique(c(1:100, which(datasets$target %in% c("bbs_rtrg_224_3", "bbs_rtrg_318_3", "bbs_rtrg_19_7", "bbs_rtrg_116_18", "bbs_rtrg_3_80")))), ]
+#
+datasets <- datasets[ which(datasets$target %in% c("bbs_rtrg_224_3", "bbs_rtrg_318_3", "bbs_rtrg_19_7", "bbs_rtrg_116_18", "bbs_rtrg_3_80")), ]
+
+ #datasets <- datasets[ which(datasets$target %in% c("bbs_rtrg_116_18", "bbs_rtrg_224_3")), ]
+
+#
+# sim_plan <- drake_plan(
+#   actual_sims = target(rwar::ssims_wrapper(dataset, simtype = "actual"),
+#                        transform = map(
+#                          dataset = !!rlang::syms(datasets$target)
+#                        )),
+#   nc_sims = target(rwar::ssims_wrapper(dataset, simtype = "nc"),
+#                    transform = map(
+#                      dataset = !!rlang::syms(datasets$target)
+#                    )),
+#   nsc_sims = target(rwar::ssims_wrapper(dataset, simtype = "nsc"),
+#                     transform = map(
+#                     ))
+# )
+
+
+methods <- drake_plan(
+  lnm = target(rwar::local_null_model_wrapper(dataset, n_null_model_sims = 500, n_isd_draws = 5, ndraws = 25, initial_null_model_seed = 1989),
+                 transform = map(
+                   dataset = !!rlang::syms(datasets$target)
+                 )),
+  lnm_s = target(rwar::summarize_null_models(lnm),
+                 transform = map(lnm)),
+  anm = target(rwar::local_null_model_wrapper(dataset, n_null_model_sims = 1, n_isd_draws = 5, ndraws = 25, initial_null_model_seed = 1989, return_actual = T),
+               transform = map(
+                 dataset = !!rlang::syms(datasets$target)
+               )),
+  anm_s = target(rwar::summarize_null_models(anm),
+                 transform = map(anm)),
+  as = target(dplyr::combine(lnm_s),
+              transform = combine(lnm_s)),
+  all_summaries = target(dplyr::bind_rows(as)),
+  aa = target(dplyr::combine(anm_s),
+              transform = combine(anm_s)),
+  all_actual_summaries = target(dplyr::bind_rows(aa))
+)
+
+all = bind_rows(datasets, methods)
+
+
+## Set up the cache and config
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("aspirational_structure", "drake_caches", "dev_lnm.sqlite"))
+cache <- storr::storr_dbi("datatable", "keystable", db)
+cache$del(key = "lock", namespace = "session")
+
+## Run the pipeline
+nodename <- Sys.info()["nodename"]
+# if(grepl("ufhpc", nodename)) {
+#    print("I know I am on the HiPerGator!")
+
+if(run_hpg) {
+  library(clustermq)
+  options(clustermq.scheduler = "multicore"#, clustermq.template = "slurm_clustermq.tmpl")
+  )
+  ## Run the pipeline parallelized for HiPerGator
+  system.time(make(all,
+                   force = TRUE,
+                   cache = cache,
+                   verbose = 1,
+                   parallelism = "clustermq",
+                   jobs = 5,
+                   caching = "main",
+                   memory_strategy = "autoclean",
+                   lock_envir = F,
+                   garbage_collection = T))# Important for DBI caches!
+} else {
+
+
+  # Run the pipeline on multiple local cores
+  system.time(make(all, cache = cache,  verbose = 1, memory_strategy = "autoclean", lock_envir = F))
+
+
+}
+
+loadd(all_summaries, cache = cache)
+save(all_summaries, file = "local_summaries.Rds")
+
+DBI::dbDisconnect(db)
+rm(cache)
+print("Completed OK")
+
