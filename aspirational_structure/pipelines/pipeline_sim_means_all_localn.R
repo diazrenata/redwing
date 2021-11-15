e78494eeae2013057ceb8e61b40cd3730fe6a214
Renata Diaz <diaz.renatam@gmail.com>
2021-11-15 08:41:29 -0500

    local nm

--- aspirational_structure/pipelines/pipeline_sim_means_all_localn.R
+++ aspirational_structure/pipelines/pipeline_sim_means_all_localn.R
@@ -7,9 +7,8 @@ library(brms)
 library(tidybayes)
 
 run_hpg = T
-max_caps <- c(75, 150, 225, 300, 375, 450, 528)
+max_caps <- c(125, 350, 528)
 for(i in 1:length(max_caps)) {
-
 datasets <- MATSS::build_bbs_datasets_plan()
 
 
@@ -21,24 +20,35 @@ datasets <- datasets[ which(datasets$target %in% working_datasets$matssname), ]
 datasets <- datasets[ unique(c(1:max_caps[i], which(datasets$target %in% c("bbs_rtrg_224_3", "bbs_rtrg_318_3", "bbs_rtrg_19_7", "bbs_rtrg_116_18", "bbs_rtrg_3_80")))), ]
 #
 #datasets <- datasets[ which(datasets$target %in% c("bbs_rtrg_224_3", "bbs_rtrg_318_3", "bbs_rtrg_19_7", "bbs_rtrg_116_18", "bbs_rtrg_3_80")), ]
-#datasets <- datasets[ which(datasets$target %in% c( "bbs_rtrg_116_18", "bbs_rtrg_224_3")), ]
+
 # datasets <- datasets[ which(datasets$target %in% c("bbs_rtrg_116_18")), ]
 
-null_seeds <- c(1989)
-
-null_dataset <- drake_plan(
-  nd = target(rwar::local_null_model(dataset, n_isd_draws = 1, ndraws = 1),
-                   transform = cross(
-                     dataset = !!rlang::syms(datasets$target),
-                     null_seed = !!null_seeds
-                   )
-))
+#
+# sim_plan <- drake_plan(
+#   actual_sims = target(rwar::ssims_wrapper(dataset, simtype = "actual"),
+#                        transform = map(
+#                          dataset = !!rlang::syms(datasets$target)
+#                        )),
+#   nc_sims = target(rwar::ssims_wrapper(dataset, simtype = "nc"),
+#                    transform = map(
+#                      dataset = !!rlang::syms(datasets$target)
+#                    )),
+#   nsc_sims = target(rwar::ssims_wrapper(dataset, simtype = "nsc"),
+#                     transform = map(
+#                     ))
+# )
+#
+# draw_wrapper <- function(winners, fits) {
+#   draws = rwar::winner_draws(winners, fits)
+#   draw_qis = rwar::winner_qis(draws)
+#   draw_qis
+# }
 
 methods <- drake_plan(
-  ssims = target(rwar::ssims_wrapper(dataset, simtype, n_isd_draws = 5, ndraws = 5),
+  ssims = target(rwar::ssims_wrapper(dataset, simtype, n_isd_draws = 1, ndraws = 1),
                  transform = cross(
-                   dataset = !!rlang::syms(null_dataset$target),
-                   simtype = c("actual")#, "nc", "nsc")
+                   dataset = !!rlang::syms(datasets$target),
+                   simtype = c("actual", "nc", "nsc")
                  ) ),
   as = target(dplyr::combine(ssims),
              transform = combine(ssims)),
@@ -55,28 +65,28 @@ methods <- drake_plan(
   aw = target(dplyr::combine(winners),
               transform = combine(winners)),
   all_winners  = target(dplyr::bind_rows(aw)),
-  # diag = target(rwar::extract_diagnostics(fits),
-  #               transform = map(fits)),
- # adg = target(dplyr::combine(diag),
- #              transform = combine(diag)),
- # all_diagnostics = target(dplyr::bind_rows(adg)),
-# draws = target(rwar::winner_draws(winners, fits),
- #               transform = map(winners, fits)),
+  diag = target(rwar::extract_diagnostics(fits),
+                transform = map(fits)),
+  adg = target(dplyr::combine(diag),
+               transform = combine(diag)),
+  all_diagnostics = target(dplyr::bind_rows(adg)),
+ # draws = target(rwar::winner_draws(winners, fits),
+ #                transform = map(winners, fits)),
   #ad = target(dplyr::combine(draws),
    #           transform = combine(draws)),
  # all_draws = target(dplyr::bind_rows(ad)),
   qis = target(rwar::draw_wrapper(winners, fits),
                transform = combine(winners, fits, .by = fits)),
- aq = target(dplyr::combine(qis),
+  aq = target(dplyr::combine(qis),
               transform = combine(qis)),
   all_qis = target(dplyr::bind_rows(aq))
 )
 
-all = bind_rows(datasets,core_datasets,  methods)
+all = bind_rows(datasets, methods)
 
 
 ## Set up the cache and config
-db <- DBI::dbConnect(RSQLite::SQLite(), here::here("aspirational_structure", "drake_caches", "all_local_n.sqlite"))
+db <- DBI::dbConnect(RSQLite::SQLite(), here::here("aspirational_structure", "drake_caches", "all.sqlite"))
 cache <- storr::storr_dbi("datatable", "keystable", db)
 cache$del(key = "lock", namespace = "session")
 
@@ -109,11 +119,14 @@ if(run_hpg) {
 
 }
 #
- loadd(all_sims, all_winners, all_qis, cache = cache)#,  all_qis, all_diagnostics, cache = cache)
- save(all_sims, all_winners , all_qis, file = "portable_results_core_null.Rds")#all_qis, all_diagnostics, file = "portable_results_core_null.Rds")
+ loadd(all_sims, all_winners,  all_qis, all_diagnostics, cache = cache)
+ save(all_sims, all_winners,  all_qis, all_diagnostics, file = "portable_results_all_localn.Rds")
 rm(all_sims)
 rm(all_winners)
+rm(all_qis)
+rm(all_diagnostics)
 DBI::dbDisconnect(db)
 rm(cache)
 print("Completed OK")
+
 }
