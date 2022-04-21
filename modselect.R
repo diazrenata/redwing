7cd6ba532f4a5937ca8bfe716354e225ccf78c34
Renata Diaz <diaz.renatam@gmail.com>
2022-04-21 11:23:23 -0400

    local change to modselect

--- modselect.R
+++ modselect.R
@@ -57,3 +57,34 @@ ggplot(filter(winning_fits, modelcomplexity > 2, model_span > 27), aes(ratio_sim
   geom_vline(xintercept = 1)
 
 mean(filter(winning_fits, modelcomplexity > 2, model_span > 27)$ratio_real >filter(winning_fits, modelcomplexity > 2, model_span > 27)$ratio_sim)
+
+loadd(all_cor_comps, cache=cache)
+
+mean(all_cor_comps$real_cor_percentile < .025)
+mean(all_cor_comps$real_cor_ses < 1.95)
+
+long_core_comps <- all_cor_comps %>%
+  select(matssname, real_cor, sim_cor_mean) %>%
+  tidyr::pivot_longer(-matssname, names_to = "source", values_to = "cor")
+
+ggplot(long_core_comps, aes(source, cor)) + geom_boxplot()
+
+ggplot(all_cor_comps, aes(real_cor)) + geom_histogram()
+ggplot(all_cor_comps, aes(sim_cor_mean, real_cor, color = real_cor_percentile < .025)) + geom_point() + geom_abline(slope = 1, intercept = 0) + xlim(-0, 1) + ylim(-.21, 1)
+summary(all_cor_comps$real_cor)
+summary(filter(all_cor_comps, real_cor_percentile < .05)$real_cor)
+
+all_cor_comps <- left_join(all_cor_comps, winning_fits)
+
+ggplot(all_cor_comps, aes(real_cor, color = model_formula)) + geom_density()
+ggplot(all_cor_comps, aes(model_formula, real_cor)) + geom_boxplot()
+
+loadd(all_sims, cache=cache)
+
+cv_sims <- all_sims %>%
+  group_by(matssname, source) %>%
+  summarize(cv = sd(total_biomass) / mean(total_biomass),
+            nvals = dplyr::n()) %>%
+  left_join(winning_fits)
+
+ggplot(cv_sims, aes(cv, color = source)) + geom_density() + facet_wrap(vars(model_formula), scales = "free", ncol = 1)
